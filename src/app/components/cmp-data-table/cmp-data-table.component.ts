2ae375690a2d54adce9ec4f07cf06a37ee4bd667
Simon Sutherland <sjds@simons-mbp.gateway.sonic.net>
2023-12-01 04:42:20 -0800

    fix
    make default table both looping columns and paginated

--- src/app/components/cmp-data-table/cmp-data-table.component.ts
+++ src/app/components/cmp-data-table/cmp-data-table.component.ts
@@ -1,22 +1,62 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {MerchandiseInfo, MERCHANT_DATA} from "../../data/items_data";
+import { AfterViewInit, Component, ViewChild} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import { MerchandiseInfo, MERCHANT_DATA } from "../../data/items_data";
 
 /**
- * @title Table with pagination
+ * @title Table looping columns, with pagination
  */
 
 @Component({
   selector: 'app-cmp-data-table',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
   templateUrl: './cmp-data-table.component.html',
   styleUrl: './cmp-data-table.component.scss'
 })
-export class CmpDataTableComponent implements AfterViewInit {
-  displayedColumns: string[] = ['sku', 'name', 'price', 'cost'];
-  dataSource = new MatTableDataSource(MERCHANT_DATA);
+export class CmpDataTableComponent {
+
+  // col as for loop
+  columns = [
+    {
+      columnDef: 'sku',
+      header: 'No.',
+      cell: (merch: MerchandiseInfo) => `${merch.sku}`,
+    },
+    {
+      columnDef: 'name',
+      header: 'Name',
+      cell: (merch: MerchandiseInfo) => `${merch.name}`,
+    },
+    {
+      columnDef: 'cost',
+      header: 'cost',
+      cell: (merch: MerchandiseInfo) => `${merch.cost}`,
+    },
+    {
+      columnDef: 'price',
+      header: 'price',
+      cell: (merch: MerchandiseInfo) => `${merch.price}`,
+    },
+    {
+      columnDef: 'brand',
+      header: 'brand',
+      cell: (merch: MerchandiseInfo) => `${merch.brand}`,
+    },
+    {
+      columnDef: 'department',
+      header: 'department',
+      cell: (merch: MerchandiseInfo) => `${merch.department}`,
+    },
+    {
+      columnDef: 'status',
+      header: 'status',
+      cell: (merch: MerchandiseInfo) => `${merch.status}`,
+    },
+  ];
+  dataSource = new MatTableDataSource<MerchandiseInfo>(MERCHANT_DATA);
+  displayedColumns = this.columns.map(c => c.columnDef);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
