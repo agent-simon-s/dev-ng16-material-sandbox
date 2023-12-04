399c3d59f6c812d37fc297c38521e7146c25d6b6
Simon Sutherland <sjds@simons-mbp.gateway.sonic.net>
2023-12-03 20:36:25 -0800

    featureadd drag and drop fucntion to column table in evolving default table

--- src/app/components/cmp-data-table/cmp-data-table.component.ts
+++ src/app/components/cmp-data-table/cmp-data-table.component.ts
@@ -1,17 +1,20 @@
 import { AfterViewInit, Component, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CdkDragDrop, CdkDrag, CdkDropList, moveItemInArray} from '@angular/cdk/drag-drop';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import { MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
 import { MerchandiseInfo, MERCHANT_DATA } from "../../data/items_data";
 
 /**
  * @title Table looping columns, with pagination
+ *  with pagination
+ *  with drag and drop columns
  */
 
 @Component({
   selector: 'app-cmp-data-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule],
+  imports: [CommonModule, MatTableModule, CdkDropList, CdkDrag, MatPaginatorModule],
   templateUrl: './cmp-data-table.component.html',
   styleUrl: './cmp-data-table.component.scss'
 })
@@ -57,6 +60,10 @@ export class CmpDataTableComponent {
   ];
   dataSource = new MatTableDataSource<MerchandiseInfo>(MERCHANT_DATA);
   displayedColumns = this.columns.map(c => c.columnDef);
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.displayedColumns, event.previousIndex, event.currentIndex);
+  }
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
