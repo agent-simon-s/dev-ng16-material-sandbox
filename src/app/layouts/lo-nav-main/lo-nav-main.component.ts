20dc5dbebe78c4b0fe8e1c45d41ee8b01eaa688c
Simon Sutherland <sjds@simons-mbp.gateway.sonic.net>
2023-12-03 20:38:44 -0800

    feature
    update side nav with new compnent links, add+style completion checkmarks, ficx attmpt for isActiveRoute incomplete

--- src/app/layouts/lo-nav-main/lo-nav-main.component.ts
+++ src/app/layouts/lo-nav-main/lo-nav-main.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../../app.routes';
 
 
 @Component({
@@ -15,16 +16,17 @@ import { RouterLink } from '@angular/router';
           <li><a routerLink="/mat-table" routerLinkActive="active" ariaCurrentWhenActive="page">MAT TABLE</a></li>
           <li><a routerLink="/mat-table/working" routerLinkActive="active" ariaCurrentWhenActive="page">MAT TABLE WORKING</a></li>
           <li>--</li>
-          <li><a routerLink="/tables" routerLinkActive="active" ariaCurrentWhenActive="page">TABLES</a></li>
-          <li><a routerLink="/tables/data" routerLinkActive="active" ariaCurrentWhenActive="page">DATA TABLE</a></li>
-          <li><a routerLink="/tables/page" routerLinkActive="active" ariaCurrentWhenActive="page">PAGING TABLE</a></li>
-          <li><a routerLink="/tables/sort" routerLinkActive="active" ariaCurrentWhenActive="page">SORT TABLE</a></li>
-          <li><a routerLink="/tables/link" routerLinkActive="active" ariaCurrentWhenActive="page">LINK TABLE</a></li>
-          <li><a routerLink="/tables/search" routerLinkActive="active" ariaCurrentWhenActive="page">SEARCH TABLE</a></li>
-          <li><a routerLink="/tables/header" routerLinkActive="active" ariaCurrentWhenActive="page">HEADER TABLE</a></li>
-          <li><a routerLink="/tables/buttons" routerLinkActive="active" ariaCurrentWhenActive="page">BUTTONS TABLE</a></li>
-          <li><a routerLink="/tables/sink" routerLinkActive="active" ariaCurrentWhenActive="page">SINK TABLE</a></li>
-          <li><a routerLink="/tables/all" routerLinkActive="active" ariaCurrentWhenActive="page">ALL TABLES</a></li>
+          <li><a routerLink="/tables" routerLinkActive="active" ariaCurrentWhenActive="page">TABLES <em>&#x2713;&#x2713;&#x2713;</em></a></li>
+          <li>-<a routerLink="/tables/data" routerLinkActive="active" ariaCurrentWhenActive="page">DATA DRIVEN</a></li>
+          <li>-<a routerLink="/tables/page" routerLinkActive="active" ariaCurrentWhenActive="page">PAGING <em>&#x2713;</em></a></li>
+          <li>-<a routerLink="/tables/sort" routerLinkActive="active" ariaCurrentWhenActive="page">SORT TABLE <em>&#x2713;</em></a></li>
+          <li>-<a routerLink="/tables/drag" routerLinkActive="active" ariaCurrentWhenActive="page">DRAGABLE <em>&#x2713;</em></a></li>
+          <li>-<a routerLink="/tables/link" routerLinkActive="active" ariaCurrentWhenActive="page">LINKABLE _</a></li>
+          <li>-<a routerLink="/tables/search" routerLinkActive="active" ariaCurrentWhenActive="page">SEARCHABLE <em>&#x2713;</em></a></li>
+          <li>-<a routerLink="/tables/header" routerLinkActive="active" ariaCurrentWhenActive="page">With HEADER __</a></li>
+          <li>-<a routerLink="/tables/buttons" routerLinkActive="active" ariaCurrentWhenActive="page">With BUTTONS __</a></li>
+          <li>-<a routerLink="/tables/sink" routerLinkActive="active" ariaCurrentWhenActive="page">With KITCHEN SINK __</a></li>
+          <li>-<a routerLink="/tables/all" routerLinkActive="active" ariaCurrentWhenActive="page">ALL</a></li>
           <li>--</li>
           <li><a routerLink="/hot-stepper" routerLinkActive="active" ariaCurrentWhenActive="page">HOT STEPPER</a></li>
         </ul>
@@ -34,5 +36,5 @@ import { RouterLink } from '@angular/router';
   styleUrl: './lo-nav-main.component.scss'
 })
 export class LoNavMainComponent {
-
+  //@Output() isActiveChange: EventEmitter<boolean>;
 }
