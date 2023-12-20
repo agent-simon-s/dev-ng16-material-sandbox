18446c21f8cab790696b8b08db72177f34234820
Simon Sutherland <sjds@simons-mbp.gateway.sonic.net>
2023-12-19 23:48:05 -0800

    add form field to top of cmp-mat-table

--- src/app/pages/page-mat-table/page-mat-table.component.ts
+++ src/app/pages/page-mat-table/page-mat-table.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { CmpMatTableComponent } from '../../components/cmp-mat-table/cmp-mat-table.component';
+import { CmpTableSearchComponent } from '../../components/cmp-table-search/cmp-table-search.component';
 
 /**
  * @title Table with columns defined using a for loop instead of statically written in the template.
@@ -12,25 +13,25 @@ import { CmpMatTableComponent } from '../../components/cmp-mat-table/cmp-mat-tab
 @Component({
   selector: 'app-page-mat-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatButtonModule, CmpMatTableComponent  ],
+  imports: [ CommonModule, MatTableModule, MatButtonModule, CmpMatTableComponent, CmpTableSearchComponent ],
   template: `
     <div>
       <h1 class="page-title">Items (Material)</h1>
       <p>
         Dolor sit amet consectetur adipisicing, elit. Sapiente consectetur atque recusandae tempora vitae eligendi voluptas harum qui placeat porro earum ex voluptate dolor suscipit error pariatur distinctio ea, omnis.
       </p>
-      <div class="table-meta-header">
-        <div class="side-a">
-          <button mat-raised-button color="primary">Secondary</button>
-        </div>
-        <div class="side-b">
-          <form>
-            <button mat-button color="primary">SEARCH</button><input>
-          </form>
-        </div>
-      </div>
+
       <h2>Default Table</h2>
-      <app-cmp-mat-table></app-cmp-mat-table>
+        <div class="table-meta-header">
+          <div class="side-a">
+            <button mat-raised-button color="primary">Primary</button>
+          </div>
+          <div class="side-b">
+            <app-cmp-table-search />
+          </div>
+       </div>
+
+      <app-cmp-mat-table />
     </div>
     <br>
   `,
