12592ac684810171e261691f30fbe2009b3b0c84
Simon Sutherland <sjds@simons-mbp.gateway.sonic.net>
2024-01-16 01:42:47 -0800

    break out page-home template into its own html file, add demo modal section passing data back with conditional rendering

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,18 +1,69 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import {FormsModule} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { CmpModalBaseComponent } from '../../components/cmp-modal-base/cmp-modal-base.component';
+
 
 @Component({
-  selector: 'app-page-home',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>
-      Hello There!
-    </h1>
-    <h2>This is a sandbox for sketching out and testing various newer angular paterns, and the latest version of angular material</h2>
-  `,
-  styleUrl: './page-home.component.scss'
+  selector: 'app-page-home',
+  styleUrl: './page-home.component.scss',
+  templateUrl: './page-home.component.html',
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule],
 })
+
 export class PageHomeComponent {
 
+
+  // placeholder for modal result
+  search: string = "";
+  // placeholder for data to modal
+  stub: string = "";
+
+  constructor(
+    public dialog: MatDialog
+  ) {}
+
+  openDialog():void {
+    const dialogRef = this.dialog.open( CmpModalBaseComponent, {
+      width: '600px',
+      height: '600px',
+      panelClass: 'my-mat-modal-mask',
+      data: { stub: 'kitkat' }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('dialog is closed');
+      console.log(`Dialog result: ${result}`);
+      this.search = result;
+    });
+  }
 }
+
+// openDialog():void {
+  // const dialogRef = this.dialog.open( CmpModalBaseComponent, {
+  //   width: '600px',
+  //   height: '600px',
+  //   panelClass: 'my-mat-modal-dialog',
+  //   data: { search: this.search, stub: this.stub }
+  // });
+
+  // dialogRef.afterClosed().subscribe((result) => {
+  //   console.log('dialog is closed');
+  //   console.log(`Dialog result: ${result}`);
+  // });
+// }
+// }
